c98c21542cd0bbb82cc88ec5e81e1423c0b8bc28
Edward Muller <edwardam@interlix.com>
2016-07-05 13:39:44 -0700

    Cleanup some stuff

--- main.go
+++ main.go
@@ -1,11 +1,31 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 )
 
+func assetDir() (string, error) {
+	d, err := os.Getwd()
+	if err != nil {
+		return d, err
+	}
+	return filepath.Join(d, "public"), nil
+}
+
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("/app/public")))
-	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	a, err := assetDir()
+	if err != nil {
+		log.Fatal("Unable to determine asset directory:", err)
+	}
+
+	p, e := os.LookupEnv("PORT")
+	if !e {
+		p = "5000" // Default port
+	}
+
+	http.Handle("/", http.FileServer(http.Dir(a)))
+	log.Println(http.ListenAndServe(":"+p, nil))
 }
